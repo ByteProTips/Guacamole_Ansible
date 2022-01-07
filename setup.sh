90bde0c934ef4978cfc74b3fce83243d90a2d689
ByteProTips <nathan.mcfadden@icloud.com>
2022-01-06 21:49:53 -0500

    Changed dnf to yum to support CentOS7

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 curl https://raw.githubusercontent.com/eslam-gomaa/mysql_secure_installation_Ansible/master/library/mysql_secure_installation.py > $SCRIPT_DIR/library/mysql_secure_installation.py
 
-dnf install -y epel-release
+yum install -y epel-release
 
-dnf install -y ansible
+yum install -y ansible
 
 ansible-galaxy collection install community.mysql community.general ansible.posix
 
