6d19a160a49a811eb8e262d55a2c321b16764dbc
ByteProTips <nathan.mcfadden@icloud.com>
2022-01-07 01:14:24 -0500

    Made passwords interactive instead of requiring them to be stored in the playbook.

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ then
 	subscription-manager repos --enable rhel-*-optional-rpms \
 	                           --enable rhel-*-extras-rpms \
 	                           --enable rhel-ha-for-rhel-*-server-rpms
-	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
 fi
 
 yum install -y epel-release
