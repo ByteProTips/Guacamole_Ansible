99aae5831e8e25491f00dcf1fadbfdb34e88e6d9
ByteProTips <nathan.mcfadden@icloud.com>
2022-01-06 22:56:56 -0500

    Changes to support RHEL

--- setup.sh
+++ setup.sh
@@ -4,9 +4,23 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 curl https://raw.githubusercontent.com/eslam-gomaa/mysql_secure_installation_Ansible/master/library/mysql_secure_installation.py > $SCRIPT_DIR/library/mysql_secure_installation.py
 
-yum install -y epel-release
+if grep -q -i "release 8" /etc/redhat-release
+then
+	subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms
+	dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+fi
 
-yum install -y ansible
+if grep -q -i "release 7" /etc/redhat-release
+then
+	subscription-manager repos --enable rhel-*-optional-rpms \
+	                           --enable rhel-*-extras-rpms \
+	                           --enable rhel-ha-for-rhel-*-server-rpms
+	yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+fi
+else
+	yum install -y epel-release
+	yum install -y ansible
+fi
 
 ansible-galaxy collection install community.mysql community.general ansible.posix
 
