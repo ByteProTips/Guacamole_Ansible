51ac01655a96cd7ae12ea8ed7f8f3eb0ce2e8738
ByteProTips <nathan.mcfadden@icloud.com>
2022-01-08 20:28:47 -0500

    RHEL 7 Fixes

--- setup.sh
+++ setup.sh
@@ -19,9 +19,9 @@ fi
 if `grep -q -i "release 7" /etc/redhat-release` && `grep -q -i -e '^Red Hat.*release.*' /etc/redhat-release`
 then
 	echo "Detected RHEL 7"
+	subscription-manager repos --enable rhel-7-server-optional-rpms
 	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
 	yum install -y ansible
-	yum --enablerepo=rhel-7-server-optional-rpms install -y freerdp-devel
 fi
 
 #Prep CentOS 8
