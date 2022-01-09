06f59cb9c6ef3d8b1f4da143619f96fc84d1936c
ByteProTips <nathan.mcfadden@icloud.com>
2022-01-09 09:06:51 -0500

    Added Comments

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Created by ByteProTips (www.byteprotips.com)
 # Prep the system to run the Ansible playbook
 # Tested on CentOS 7, RHEL 7, Oracle Linux 7, Rocky Linux 8, RHEL 8, Oracle Linux 8
 
